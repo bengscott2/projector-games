50142eca2b0a3c7a8f5da3dad3ffbb1a67b23a2c
Ben Scott <bengscott2@gmail.com>
2019-08-04 18:46:42 +0100

    chore: add player functionality

---
+++ player/src/components/Player.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button v-on:click="click()">Click</button>
+  </div>
+</template>
+
+<script>
+import  io from 'socket.io-client';
+export default {
+  name: 'Player',
+  created() {
+    this.socket = io("http://192.168.1.89:3000/");
+  },
+  methods: {
+    click() {
+      this.socket.emit('click')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
