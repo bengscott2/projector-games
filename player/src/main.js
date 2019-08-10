48bf741cec80c181e5c7596a20e3c19858a3afb0
Ben Scott <bengscott2@gmail.com>
2019-08-10 10:43:38 +0100

    chore: downgrade socket.io to more stable version

--- player/src/main.js
+++ player/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+// imports root Vue component named App
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// here is where we declare our Vue application
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+// above ^ is where we mount our vue to the #app element in the public html
